dbab2f2b5d44cb52de2e6415e9fd30acf77f6906
ooeunz <yuns994@gmail.com>
2020-02-25 21:44:42 +0900

    Using YOUTUBE API

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Grid } from '@material-ui/core';
 
 import youtube from './api/youtube';
-import SearchBar from './components/SearchBar'
-import VideoList from './components/VideoList';
-import VideoDetail from './components/VideoDetail';
+import { SearchBar, VideoList, VideoDetail } from './components'
 
 const App: React.FC = () => {
+  const [video, setVideo] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState({});
+
+  const handleSubmit = async (searchTerm: string) => {
+    const response = await youtube.get('search', {
+      params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: '', /* TODO: put in YOUTUBE API KEY */
+        q: searchTerm,
+      }
+    });
+
+
+    /* setState가 안되는 버그 있음. */
+
+    console.log(response.data.items);
+
+    // setVideo(response.data.items);
+    setSelectedVideo(response.data.items[0]);
+
+    console.log(video);
+    console.log(selectedVideo);
+    
+  }
+
   return (
     <Grid justify="center" container spacing={10}>
-      <Grid item xs={8}>
+      <Grid item xs={12}>
         <Grid container spacing={10}>
-          <Grid item xs={8}>
-            <SearchBar />
+          <Grid item xs={12}>
+            <SearchBar onFormSubmit={handleSubmit} />
           </Grid>
-          <Grid item xs={6}>
+          <Grid item xs={8}>
             <VideoList />
           </Grid>
           <Grid item xs={4}>
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { Paper, TextField } from '@material-ui/core';
 
 interface SearchBarProps {
-
+  onFormSubmit(searchTerm: string): void
 }
 
-const SearchBar: React.FC<SearchBarProps> = () => {
+const SearchBar: React.FC<SearchBarProps> = ({ onFormSubmit }) => {
+  const [searchTerm, setSearchTerm] = useState<string>('');
+
+  const handleSubmit = (e: React.FormEvent) => {
+    onFormSubmit(searchTerm);
+    e.preventDefault();
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+  }
+
   return (
-    <div>This is component</div>
+    <Paper elevation={8} style={{ padding: '25px' }}>
+      <form onSubmit={handleSubmit}>
+        <TextField fullWidth label="Search..." onChange={handleChange} />
+      </form>
+    </Paper>
   );
 }
 
